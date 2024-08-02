ff8b05d2fdf02a84ce982d73ce762fb0bf1be577
Roman Kerimov <roma.kerimov@icloud.com>
2024-08-02 10:22:23 +0600

    Remove comment

--- Tests/LingvistikaTests/LingvistikaTests.swift
+++ Tests/LingvistikaTests/LingvistikaTests.swift
@@ -2,6 +2,5 @@ import Testing
 @testable import Lingvistika
 
 @Test func bzip2Version() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     #expect(bzip2Version.isEmpty == false)
 }
