bc545b8dce07f9cf9fe55f84a57a97e5685ab7f7
Roman Kerimov <roma.kerimov@icloud.com>
2024-08-02 10:14:23 +0600

    Import BZip2

--- Package.swift
+++ Package.swift
@@ -16,10 +16,15 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Lingvistika"),
+            name: "Lingvistika",
+            dependencies: ["BZip2"]
+        ),
         .testTarget(
             name: "LingvistikaTests",
             dependencies: ["Lingvistika"]
+        ),
+        .systemLibrary(
+            name: "BZip2"
         ),
     ]
 )
--- Tests/LingvistikaTests/LingvistikaTests.swift
+++ Tests/LingvistikaTests/LingvistikaTests.swift
@@ -1,6 +1,7 @@
 import Testing
 @testable import Lingvistika
 
-@Test func example() async throws {
+@Test func bzip2Version() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    #expect(bzip2Version.isEmpty == false)
 }
